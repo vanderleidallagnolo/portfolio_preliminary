c64ee2b8067445f66e429fef42e0df88754d933c
Vanderlei Dallagnolo <vanderlei.dallagnolo@ellevo.com>
2023-03-22 01:45:55 -0300

    ...

--- js/menu_tree_view.js
+++ js/menu_tree_view.js
@@ -15,6 +15,7 @@ const SCD_START_PROJECTS_ELLEVO_TRANSLATEEPRESSIONSCHATGPT	            =	3;
 const SCD_START_PROJECTS_ELLEVO_AUDIT_DB                                =	4;
 const SCD_START_WEB_JAVASCRIPT_DRAG_BORDER					            =	5;
 const SCD_START_PROJECTS_CCMS_THUMBNAIL_CREATOR					        =	6;
+const SCD_START_WEB_JAVASCRIPT_SHOWFUNCTION_SOURCEANDRESULT             =   7;
 
 function fShowContentDetail(pContentDetailId)
 {
@@ -22,35 +23,39 @@ function fShowContentDetail(pContentDetailId)
     switch(pContentDetailId)
     {
     
-        case SCD_START_WEB_JAVASCRIPT_MOUSEDRAGGING:
+        case SCD_START_WEB_JAVASCRIPT_MOUSEDRAGGING                         :
 
-            window.open("example_mouse_drag_element.html"	);
+            window.open("example_mouse_drag_element.html"	                );
             break;
                                 
-        case SCD_START_WEB_JAVASCRIPT_MENUJSTREE:
-            window.open("menu_js_tree_example.html"			);
+        case SCD_START_WEB_JAVASCRIPT_MENUJSTREE                            :
+            window.open("menu_js_tree_example.html"			                );
             break;
 
-        case SCD_START_WEB_TEMPLATE_LOGINSWANWICH:
-            window.open("swanwich_login_example.html"		);
+        case SCD_START_WEB_TEMPLATE_LOGINSWANWICH                           :
+            window.open("swanwich_login_example.html"		                );
             break;
 
-        case SCD_START_PROJECTS_ELLEVO_TRANSLATEEPRESSIONSCHATGPT:
-            window.open("translate_db_expressions_chatgpt.html"		);
+        case SCD_START_PROJECTS_ELLEVO_TRANSLATEEPRESSIONSCHATGPT           :
+            window.open("translate_db_expressions_chatgpt.html"		        );
             break;
 
-        case SCD_START_PROJECTS_ELLEVO_AUDIT_DB:
-            window.open("audit_db.html"		);
+        case SCD_START_PROJECTS_ELLEVO_AUDIT_DB                             :
+            window.open("audit_db.html"		                                );
             break;
             
-        case SCD_START_WEB_JAVASCRIPT_DRAG_BORDER:
-            window.open("div_handler_drag_border.html"		);
+        case SCD_START_WEB_JAVASCRIPT_DRAG_BORDER                           :
+            window.open("div_handler_drag_border.html"		                );
             break;
 
-        case SCD_START_PROJECTS_CCMS_THUMBNAIL_CREATOR:
-            window.open("thumbnail_creator.html"		);
+        case SCD_START_PROJECTS_CCMS_THUMBNAIL_CREATOR                      :
+            window.open("thumbnail_creator.html"		                    );
             break;
-                        
+
+        case SCD_START_WEB_JAVASCRIPT_SHOWFUNCTION_SOURCEANDRESULT          :
+            window.open("show_function_source_and_result.html"		        );
+            break;
+                                    
         default:
             break;
     
